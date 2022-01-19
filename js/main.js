30ae4869daea6a2615684795135832ffa0bdbaf1
Evan <39568306+Evanlyboy@users.noreply.github.com>
2022-01-19 12:16:33 -0500

    setup

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// this is a self-invoking anonymous function
+// it is also called a module (my variation of the module pattern)
+// it encapsulates all of your custom code and makes it private
+
+(() => {
+ // make associations (select) with elements in the HTML markup using CSS
+ let theHeading = document.querySelector(".main-heading");
+
+ // script your behaviour with functions
+ function logElement () {
+     console.log("clicked on the heading");
+ }
+
+ // add event handling here (user "triggers")
+ // assign the event you want to listen for, and the function that should
+ theHeading.addEventListener("click", logElement);
+}) ();
