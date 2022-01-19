7dcf4cf4c4a2c5bdbe904605dde39468427cb556
Evan <39568306+Evanlyboy@users.noreply.github.com>
2022-01-19 14:45:45 -0500

    added JS

--- js/main.js
+++ js/main.js
@@ -4,14 +4,22 @@
 
 (() => {
  // make associations (select) with elements in the HTML markup using CSS
- let theHeading = document.querySelector(".main-heading");
+ let theHeading = document.querySelector(".main-heading"),
+    theIcons = document.querySelectorAll(".icon");
+
+    // debugger; // this is like a breakpoint / pause point in the code
+
 
  // script your behaviour with functions
  function logElement () {
-     console.log("clicked on the heading");
+    //  the "this" keyword refers to the thing that invokes the function
+    // so the "thing" that you click on, mouse over, interact with on the page
+     console.log("clicked on an element:", this.id);
  }
 
  // add event handling here (user "triggers")
  // assign the event you want to listen for, and the function that should
  theHeading.addEventListener("click", logElement);
+//  one to many relationship (selecting a group of elements) - you have to process this collection and work with each element sequentially
+ theIcons.forEach(icon => icon.addEventListener("click", logElement))
 }) ();
